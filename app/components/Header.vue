ddd826eef338b46a0f7e07a623c3223f52559470
Jack Kufa <jack@kufa.io>
2024-03-26 22:04:22 -0400

    Forgot Echarts shoutout!

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
   <header class="px-8 py-4 bg-white">
-    <span class="logo font-black text-3xl">Kufalytics</span>
+    <span class="logo font-bold text-3xl">Kufalytics</span>
   </header>
 </template>
